0aa4ba77c38bbc5c02c64332332f5d4db7792b04
Level Drain <drain2291@gmail.com>
2020-08-15 13:14:42 +0900

    セクションの表示機能とtransitionの設定

---
+++ src/components/SlideSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="section">
+    <div class="section">
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <p>hello</p><br>
+      <button @click="$emit('close-section')">close</button>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "SlideSection",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.section {
+  z-index: 200;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.section-enter-active, .section-leave-active {
+  transition: opacity .5s;
+}
+
+.section-enter, .section-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="container">
-    <div class="position-relative d-flex justify-content-center">
-      <BubbleEffect class="position-fixed w-100"/>
-      <ul class="main-menu">
-        <li>スキル</li>
-        <li>プロフ</li>
-        <li>成果物</li>
-      </ul>
-    </div>
+  <div class="position-relative d-flex justify-content-center">
+    <BubbleEffect class="position-fixed w-100"/>
+    <SlideSection v-show="showSection" v-on:close-section="closeSection"/>
+    <ul class="main-menu">
+      <li @click="openSection">スキル</li>
+      <li>プロフ</li>
+      <li>成果物</li>
+    </ul>
   </div>
 </template>
 
 <script>
 
 import BubbleEffect from '@/components/Bubble.vue'
+import SlideSection from '@/components/SlideSection.vue'
 
 export default {
   name: 'Home',
   components: {
-    BubbleEffect
+    BubbleEffect,
+    SlideSection
+  },
+  data() {
+    return {
+      showSection: false
+    }
+  },
+  methods: {
+    openSection() {
+      this.showSection = true
+    },
+    closeSection() {
+      this.showSection = false
+    }
   }
 }
 </script>
