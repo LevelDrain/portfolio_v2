88c448bc3d2baf449c75b9200dd24e7b99b87309
LevelDrain <drain2291@gmail.com>
2021-02-01 02:38:19 +0900

    リンターのエラー修正と、ビルド設定1

--- src/router/index.js
+++ src/router/index.js
@@ -1,93 +1,93 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import $store from "@/store/index"
+import $store from '@/store/index'
 import {setTitle} from '@/mixins'
 import Home from '@/views/Home'
-import Article1 from "@/components/articles/Article1";
-import Article2 from "@/components/articles/Article2";
-import Article3 from "@/components/articles/Article3";
-import Article4 from "@/components/articles/Article4";
+import Article1 from '@/components/articles/Article1'
+import Article2 from '@/components/articles/Article2'
+import Article3 from '@/components/articles/Article3'
+import Article4 from '@/components/articles/Article4'
 
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-        meta: {
-            title: 'ホーム',
-            desc: 'ホーム'
-        },
-
-        children: [
-            {
-                path: 'article1',
-                component: Article1,
-                meta: {
-                    title: 'スキル',
-                    desc: 'スキルとピックアップ作品'
-                },
-            },
-            {
-                path: 'article2',
-                component: Article2,
-                meta: {
-                    title: 'プロフィール',
-                    desc: '作者 Level Drain についてのプロフィール'
-                },
-            },
-            {
-                path: 'article3',
-                component: Article3,
-                meta: {
-                    title: 'ポートフォリオについて',
-                    desc: 'このポートフォリオについて'
-                },
-            },
-            {
-                path: 'article4',
-                component: Article4,
-                meta: {
-                    title: '前田デザイン室の活動',
-                    desc: 'オンラインサロン前田デザイン室での活動'
-                },
-            },
-        ]
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: 'ホーム',
+      desc: 'ホーム'
     },
-    //{
-    /* * route level code-splitting
+
+    children: [
+      {
+        path: 'article1',
+        component: Article1,
+        meta: {
+          title: 'スキル',
+          desc: 'スキルとピックアップ作品'
+        },
+      },
+      {
+        path: 'article2',
+        component: Article2,
+        meta: {
+          title: 'プロフィール',
+          desc: '作者 Level Drain についてのプロフィール'
+        },
+      },
+      {
+        path: 'article3',
+        component: Article3,
+        meta: {
+          title: 'ポートフォリオについて',
+          desc: 'このポートフォリオについて'
+        },
+      },
+      {
+        path: 'article4',
+        component: Article4,
+        meta: {
+          title: '前田デザイン室の活動',
+          desc: 'オンラインサロン前田デザイン室での活動'
+        },
+      },
+    ]
+  },
+  //{
+  /* * route level code-splitting
     * this generates a separate chunk (about.[hash].js) for this route
     * which is lazy-loaded when the route is visited.
     * */
 
-    //実装完了まで廃止
-    //path: '/works',
-    //name: 'Works',
-    //component: () => import(/* webpackChunkName: "about" */ '../views/Works.vue')
-    //},
+  //実装完了まで廃止
+  //path: '/works',
+  //name: 'Works',
+  //component: () => import(/* webpackChunkName: "about" */ '../views/Works.vue')
+  //},
 ]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
 
 router.beforeEach((to, from, next) => {
-    setTitle(to.meta.title)
-    next()
+  setTitle(to.meta.title)
+  next()
 })
 
 router.afterEach((to) => {
-    if (to.path !== '/') {
-        //Homeからrouter経由であればstoreにアクセスできないためwatch使わない
-        //url変更と同時に開く処理をstoreに転送
-        $store.commit('openSection');
-    } else {
-        //ホームに戻る場合に閉じさせる
-        $store.commit('closeSection');
-    }
-});
+  if (to.path !== '/') {
+    //Homeからrouter経由であればstoreにアクセスできないためwatch使わない
+    //url変更と同時に開く処理をstoreに転送
+    $store.commit('openSection')
+  } else {
+    //ホームに戻る場合に閉じさせる
+    $store.commit('closeSection')
+  }
+})
 
 export default router
---
+++ vue.config.js
@@ -0,0 +1,59 @@
+//const path = require('path')
+//const PrerenderSPAPlugin = require('prerender-spa-plugin')
+//const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
+module.exports = {
+  publicPath: './',
+  /*publicPath: process.env.NODE_ENV === 'production'
+      ? '/production-sub-path/'
+      : '/',
+  pages: {
+      index: {
+          // entry for the page
+          entry: 'src/main.js',
+          // the source template
+          template: 'public/index.html',
+          // output as dist/index.html
+          filename: 'index.html',
+          // when using title option,
+          // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+          title: '上島研究所',
+          // chunks to include on this page, by default includes
+          // extracted common chunks and vendor chunks.
+          chunks: ['chunk-vendors', 'chunk-common', 'index']
+      },
+      // when using the entry-only string format,
+      // template is inferred to be `public/subpage.html`
+      // and falls back to `public/index.html` if not found.
+      // Output filename is inferred to be `subpage.html`.
+      //subpage: this.$router.path
+
+      //https://cli.vuejs.org/config/#publicpath
+      //https://abierre.com/article/5b28fd48ef2031a4e3025daf
+      //https://cli.vuejs.org/config/#lintonsave
+  },*/
+
+  // configureWebpack: () => {
+  //     if (process.env.NODE_ENV === 'production') {
+  //         return {
+  //             plugins: [
+  //                 new PrerenderSPAPlugin({
+  //                     staticDir: path.join(__dirname, 'dist'),
+  //                     routes: [
+  //                         '/',
+  //                         '/article1',
+  //                         '/article2',
+  //                         '/article3',
+  //                         '/article4'
+  //                     ],
+  //                     renderer: new Renderer({
+  //                         renderAfterDocumentEvent: 'x-app-rendered',
+  //                         renderAfterTime: 3000,
+  //                         headless: true
+  //                     })
+  //                 }),
+  //             ]
+  //         }
+  //     }
+  // },
+}
